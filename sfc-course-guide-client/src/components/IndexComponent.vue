8b6a6fa2e4611c28ea8e59a5f197c0b7ed4220f6
LuckyWind_sck <luckywindsck@gmail.com>
2019-12-05 16:27:44 +0900

    feat: change icon and position of themeSwitcher

--- sfc-course-guide-client/src/components/IndexComponent.vue
+++ sfc-course-guide-client/src/components/IndexComponent.vue
@@ -1,6 +1,51 @@
 <template>
   <div :class="{'dark': dark}" class="index">
-    <button @click="themeSwitcher">{{ this.switcherName }}</button>
+    <span class="themeSwitcher" @click="themeSwitcher">
+      <svg
+        v-if="this.dark"
+        xmlns="http://www.w3.org/2000/svg"
+        width="24"
+        height="24"
+        viewBox="0 0 24 24"
+        fill="none"
+        stroke="currentColor"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        class="feather feather-sunrise"
+      >
+        <path d="M17 18a5 5 0 0 0-10 0" />
+        <line x1="12" y1="2" x2="12" y2="9" />
+        <line x1="4.22" y1="10.22" x2="5.64" y2="11.64" />
+        <line x1="1" y1="18" x2="3" y2="18" />
+        <line x1="21" y1="18" x2="23" y2="18" />
+        <line x1="18.36" y1="11.64" x2="19.78" y2="10.22" />
+        <line x1="23" y1="22" x2="1" y2="22" />
+        <polyline points="8 6 12 2 16 6" />
+      </svg>
+      <svg
+        v-else
+        xmlns="http://www.w3.org/2000/svg"
+        width="24"
+        height="24"
+        viewBox="0 0 24 24"
+        fill="none"
+        stroke="currentColor"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        class="feather feather-sunset"
+      >
+        <path d="M17 18a5 5 0 0 0-10 0" />
+        <line x1="12" y1="9" x2="12" y2="2" />
+        <line x1="4.22" y1="10.22" x2="5.64" y2="11.64" />
+        <line x1="1" y1="18" x2="3" y2="18" />
+        <line x1="21" y1="18" x2="23" y2="18" />
+        <line x1="18.36" y1="11.64" x2="19.78" y2="10.22" />
+        <line x1="23" y1="22" x2="1" y2="22" />
+        <polyline points="16 5 12 9 8 5" />
+      </svg>
+    </span>
     <router-view></router-view>
   </div>
 </template>
@@ -21,8 +66,16 @@ body.dark {
   justify-content: center;
   text-align: center;
 }
-button {
-  margin: 20px 0% 0% 70%;
+.themeSwitcher {
+  position: absolute;
+  color: #ff6200;
+  top: 1.5em;
+  right: 1.5em;
+  cursor: pointer;
+}
+svg {
+  width: 2em;
+  height: 2em;
 }
 </style>
 
